ad8ec31e80a685e837c35ff0ab36c251800f3cae
Goran P <nallenrockar123@gmail.com>
2024-04-05 15:29:22 +0200

    flytt av lista som skall synas i NavBaren

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
 import Navbar from './components/Navbar/NavBar';
 
+const GoToNavLink = [
+  { Text: 'Home', href: '/#' },
+  { Text: 'About oss', href: '/#' },
+  { Text: 'Contakt oss', href: '/#' },
+  { Text: 'Different services', href: '/#' },
+  { Text: 'Cooperate with oss', href: '/#' }
+];
 
 function App() {
-
   return (
     <>
-    <Navbar />
+      <Navbar GoToNavLink={GoToNavLink} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
