b445753a4e8bda3cf2318b1a4c8457ef0b1be04f
Lucifer <nallenrockar123@gmail.com>
2024-04-09 19:27:25 +0200

    testar en ny funktion bild  carusel

---
+++ src/components/BildCarusel/BildCarusel.tsx
@@ -0,0 +1,10 @@
+<div id="image-track">
+    <img src="./img/bilder-bilar/alfa.jpg" className="image" />
+    <img src="./img/bilder-bilar/alfa.jpg" className="image" />
+    <img src="./img/bilder-bilar/alfa.jpg" className="image" />
+    <img src="./img/bilder-bilar/alfa.jpg" className="image" />
+    <img src="./img/bilder-bilar/alfa.jpg" className="image" />
+    <img src="./img/bilder-bilar/alfa.jpg" className="image" />
+    <img src="./img/bilder-bilar/alfa.jpg" className="image" />
+    <img src="./img/bilder-bilar/alfa.jpg" className="image" />
+</div>
