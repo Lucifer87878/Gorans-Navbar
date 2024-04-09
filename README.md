6d04e41df4750dc0a66c1caa28fa3c39fb6b52f7
Lucifer <nallenrockar123@gmail.com>
2024-04-09 22:22:55 +0200

    tog bort bil karusel och gorde media knapp meny

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-## Information if you want to use all the icons intended for this Navbar
+# Information if you want to use all the icons intended for this Navbar
 * You need to install a patch which is
 
 - * npm install react-icons
 
-# then import from the following link
+## then import from the following link
 * import { FaBars, FaTimes } from "react-icons/fa";
 
 
@@ -11,4 +11,10 @@
 - if you want to check more icons, go to =>
 https://react-icons-v2.vercel.app/
 
+**********************************************************************************
 
+
+# If you want to use the share button code. 
+
+* and you want to change the link where it should go and if you want to change icons, I recommend that you check this link => 
+https://remixicon.com/
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css';
 import Navbar from './components/Navbar/NavBar';
-import Card from './components/Cards/Card';
+import ShareButton from './components/Buttens/buttens';
+// import Card from './components/Cards/Card';
+
 
 const GoToNavLink = [
   { Text: 'Home', href: '/#' },
@@ -10,10 +12,22 @@ const GoToNavLink = [
   { Text: 'Cooperate with oss', href: '/#' }
 ];
 
+const socialLinks = [
+  { className: "twitter", icon: "ri-twitter-line", href: "#" },
+  { className: "facebook", icon: "ri-facebook-line", href: "#" },
+  { className: "close", icon: "ri-close-line", href: "#" }, // För att stänga knap menyn 
+  { className: "dribbble", icon: "ri-dribbble-line", href: "#" },
+  { className: "whatsapp", icon: "ri-whatsapp-line", href: "#" }
+];
+
+
+
+      
 function App() {
   return (
     <>
       <Navbar GoToNavLink={GoToNavLink} />
+      <ShareButton socialLinks={socialLinks} />
       {/* <Card /> */}
     </>
   );
